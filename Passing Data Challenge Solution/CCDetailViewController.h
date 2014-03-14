8e9ef5a5f79c83502998ca0757bd787e7410ce6b
Caleb <hktan_MBP@sarahsmacbook2.lan>
2014-03-14 08:25:36 +0100

    first commit

---
+++ Passing Data Challenge Solution/CCDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCDetailViewController.h
+//  Passing Data Challenge Solution
+//
+//  Created by Caleb on 13/03/14.
+//  Copyright (c) 2014 Caleb Tan. All rights reserved.
+//
+
+#import "CCViewController.h"
+
+@interface CCDetailViewController : CCViewController
+
+@end
